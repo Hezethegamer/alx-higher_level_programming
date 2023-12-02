6a19100996d56c010aaef05ef9911d342583963f
hezethegamer <hezethegamer01@gmail.com>
2023-12-02 15:24:34 -0800

    bnj

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-""" Python script that takes in a URL and an email, sends a POST request to
-the passed URL with the email as a parameter, and displays the body of the
-response (decoded in utf-8)"""
+"""
+Python script that takes in a URL and an email, sends a POST
+request to the passed URL with the email as a parameter, and
+displays the body of the response
+"""
+from urllib import request, parse
+import sys
 
-from urllib.request import Request, urlopen
-import urllib.parse
-from sys import argv
 
 if __name__ == "__main__":
-    values = {'email': argv[2]}
-    data = urllib.parse.urlencode(values).encode('utf-8')
-    with urlopen(Request(argv[1], data)) as resp:
-        print(resp.read().decode('utf-8'))
+    values = {'email': sys.argv[2]}
+    data = parse.urlencode(values)
+    data = data.encode('ascii')
+    req = request.Request(sys.argv[1], data)
+    with request.urlopen(req) as response:
+        body = response.read()
+        print(body.decode('utf-8'))
