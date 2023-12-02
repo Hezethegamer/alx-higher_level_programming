609cc411275c4a625b857a635ce46cde07d202d0
hezethegamer <hezethegamer01@gmail.com>
2023-12-02 15:31:38 -0800

    bbh

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-""" Takes in a URL, sends a request to the URL and displays
-the body of the response.(error text) """
+"""sends a request to the URL and displays the body of the response."""
+
 import requests
 import sys
 
 if __name__ == "__main__":
-    resp = requests.get(sys.argv[1])
 
-    if str(resp.status_code)[0] == '2':
-        print(resp.text)
+    req = requests.get(sys.argv[1])
+
+    if req.status_code >= 400:
+        print("Error code:", req.status_code)
     else:
-        print("Error code: {}".format(resp.status_code))
+        print(req.text)
