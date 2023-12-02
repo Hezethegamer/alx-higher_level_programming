5c2eb534cad010b373aa0bf59e8c3c82b99749ef
hezethegamer <hezethegamer01@gmail.com>
2023-12-02 15:21:00 -0800

    hh

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """
-Script that takes in a URL, sends a request to the URL and displays the value
-of the X-Request-Id variable found in the header of the response.
+Python script that takes in a URL, sends a request and displays
+the value of the X-Request-Id variable found in the header
 """
-from urllib.request import Request, urlopen
-from sys import argv
+import urllib.request
+import sys
+
 
 if __name__ == "__main__":
-    with urlopen(Request(argv[1])) as resp:
-        print(resp.headers.get('X-Request-Id'))
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        html = response.info()
+        print(html.get('X-Request-Id'))
