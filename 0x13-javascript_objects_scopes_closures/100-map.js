bc2942bd2bdacb9c30f649ff5b7a935b2fa8ee06
hezethegamer <hezethegamer01@gmail.com>
2023-12-03 11:16:08 -0800

    gb

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
+/* a script that imports an array and computes a new array */
 const list = require('./100-data').list;
-const mapped = list.map(function (value, index) { return value * index; });
+
+const newList = list.map((el, idx) => el * idx);
 console.log(list);
-console.log(mapped);
+console.log(newList);
