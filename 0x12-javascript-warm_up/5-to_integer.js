08d78b18d87a70d9416b916e349688d8d1715d3d
hezethegamer <hezethegamer01@gmail.com>
2023-12-05 15:13:56 -0800

    hh

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,11 +1,6 @@
 #!/usr/bin/node
-const arg = process.argv[2]; // Get the first argument from the command line
-
-const num = parseInt(arg); // Attempt to convert the argument to an integer
-
-if (!isNaN(num)) {
-  console.log(`My number: ${num}`); // Print if the argument can be converted to an integer
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+  console.log('Not a number');
 } else {
-  console.log('Not a number'); // Print if the argument can't be converted to an integer
+  console.log(`My number: ${parseInt(process.argv[2])}`);
 }
-
