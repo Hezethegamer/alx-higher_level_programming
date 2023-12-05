ed2467a6209166d9021b32c2d9bb3018fbef93a1
hezethegamer <hezethegamer01@gmail.com>
2023-12-05 14:55:54 -0800

    hh

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-const argv = process.argv;
-if (argv.length === 2) {
+if (process.argv[2] === undefined) {
   console.log('No argument');
-} else if (argv.length === 3) {
-  console.log('Argument found');
-} else {
+} else if (process.argv[3] !== undefined) {
   console.log('Arguments found');
+} else {
+  console.log('Argument found');
 }
