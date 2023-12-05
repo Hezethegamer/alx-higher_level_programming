fb6803a35e3480717b9de5100ea1272ef4775b22
hezethegamer <hezethegamer01@gmail.com>
2023-12-05 15:18:18 -0800

    hhh

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,5 +1,11 @@
 #!/usr/bin/node
-const factorial = n => isNaN(n) || n === 0 ? 1 : n * factorial(n - 1);
-const [arg1] = process.argv.slice(2);
-const num = parseInt(arg1);
-console.log(factorial(num));
+function fuct (n) {
+  if (n < 0) {
+    return (-1);
+  }
+  if (n === 0 || isNaN(n)) {
+    return (1);
+  }
+  return (n * fuct(n - 1));
+}
+console.log(fuct(Number(process.argv[2])));
